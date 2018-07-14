5f7f05b9e1af46ee8b56e6fa558c01cc1d38651f
samir82show <samir82show@hotmail.com>
2018-07-14 17:27:13 +0300

    first commit

---
+++ build/generated-sources/ap-source-output/entity/domain/Simple_.java
@@ -0,0 +1,14 @@
+package entity.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-14T17:16:56")
+@StaticMetamodel(Simple.class)
+public class Simple_ { 
+
+    public static volatile SingularAttribute<Simple, String> name;
+    public static volatile SingularAttribute<Simple, Long> id;
+
+}
